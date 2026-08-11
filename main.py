e51294a1d9ae1405530cd3b728628dc62be32f02
StuckStudd <developmentartem096@gmail.com>
2026-08-11 16:34:33 +0200

    Update 1.1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from colorama import Style, Fore
 
 # commands 
-command_list = ['!help', '!set_money', '!my_purchases', '!my_transactions']
+command_list = ['!help', '!set_money', '!my_purchases', '!my_transactions', '!reset_money', '!balance', '!clear_transactions']
 
 # Opening a JSON file
 with open("data.json", "r") as f:
@@ -33,7 +33,7 @@ while True:
     clear_console()
     print(Fore.GREEN + f"Your current balance : {data.get('money', 0)}" + Style.RESET_ALL)
     print("###########")
-    print("Current command : !help !set_money !my_purchases, !my_transactions,")
+    print("Current command : !help !set_money !my_purchases, !my_transactions, !reset_money, !clear_transactions, !exit")
     
     users_commd = input("-->> ").lower().strip()
 
@@ -42,10 +42,11 @@ while True:
         print(Fore.YELLOW + "command !set_money Sets a balance for you that you'll spend, and all transactions will be recorded" + Style.RESET_ALL)
         print(Fore.YELLOW + "command !my_transactions will show you all your transactions" + Style.RESET_ALL)
         print(Fore.YELLOW + "command !my_purchases You can add what you bought, for example what you bought and how much you spent" + Style.RESET_ALL)
+        print(Fore.YELLOW + "command !reset_money resets your money" + Style.RESET_ALL)
         time.sleep(6.2)
         continue
 
-    if users_commd == command_list[1]:
+    elif users_commd == command_list[1]:
         try: 
             print("Input how much money do you have")
             set_money = float(input("-->> "))
@@ -56,7 +57,7 @@ while True:
             time.sleep(4.3)
 
     
-    if users_commd == command_list[2]:
+    elif users_commd == command_list[2]:
         print("Write what you bought")
         bought_item = input("-->> ")
 
@@ -78,9 +79,43 @@ while True:
             time.sleep(4.3)
 
 
-    if users_commd == command_list[3]:
+    elif users_commd == command_list[3]:
         clear_console()
         print(Fore.GREEN + "--- Your transactions ---" + Style.RESET_ALL)
         print(f"Purchased items: {data['transactions']['acquired']}")
         print(f"Spent amounts: {data['transactions']['spent']}")
         input("\nPress Enter to continue...")
+
+
+    elif users_commd == command_list[4]:
+        clear_console()
+        confirm = input('Are you sure? y/n -> ').strip().lower()
+
+        if confirm == "y":
+            data['money'] = 0
+            save_data()
+            print("Saved.")
+            continue
+        
+        elif confirm == "n":
+            continue
+            
+ 
+    elif users_commd == command_list[5]:
+        clear_console()
+        print(Fore.GREEN + f"Balance : {data['money']}")
+        time.sleep(5)
+        continue
+
+    elif users_commd == command_list[6]:
+        data['transactions']['acquired'].clear()
+        data['transactions']['spent'].clear()
+        print(Fore.GREEN + "Success!" + Style.RESET_ALL)
+        save_data()
+        time.sleep(1.2)
+        continue
+        
+    elif users_command == command_list[7]:
+        print("See you later..")
+        time.sleep(2)
+        break
